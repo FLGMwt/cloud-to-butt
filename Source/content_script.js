c9e375eb6323bb551a0b44943e482926b4811e45
Ryan Stelly <ryan.stelly@live.com>
2020-01-29 09:02:03 -0600

    for nic

--- Source/content_script.js
+++ Source/content_script.js
@@ -39,10 +39,9 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\b as \b/g, " ass ");
+	v = v.replace(/\bAs \b/g, "Ass ");
+	v = v.replace(/\b AS \b/g, " ASS ");
 	
 	textNode.nodeValue = v;
 }
